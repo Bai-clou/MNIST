1ea8e82fafcb407df6d1992dd40f4631e4b6b19d
BAIJINQI <27864035226@qq.com>
2025-03-10 17:34:47 +0800

    MNIST

---
+++ main.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+from torch.nn import Sequential
+from matplotlib import pyplot as plt
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+from torchvision.utils import make_grid
+import torchvision.transforms as transforms
